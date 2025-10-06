1141f0cbbc368d947485e736f7f9c686a352f1fa
Hanna Lakus <annalakus@ukr.net>
2025-10-06 20:16:41 +0100

    change

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ export const setupServer = () => {
       credentials: true,
       origin: [
         // env('APP_DOMAIN'),
-        'https://bookings-app-frontend.vercel.app/',
+        'https://bookings-app-frontend.vercel.app',
         'http://localhost:5173',
       ],
     }),
